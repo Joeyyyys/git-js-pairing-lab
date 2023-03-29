e460a351b27f94d019b4e063a729a31ddea768e4
Joeyyyys <JosephGiler2@gmail.com>
2023-03-29 18:01:29 +0000

    made a for loop where i starts at 5 and i increases by 1 until it reaches 100

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Code your solutions in this file
+//1. Write a function `fiveToOneHundred` that console logs all numbers from 5 to 100.//Code your solutions in this file
+function fiveToHundred(){
+    let i 
+    for (i=5; i<=100; i=i+1)
+    {
+        console.log(i)
+    }
+ }
+ console.log(fiveToHundred())
+ 
+ 
