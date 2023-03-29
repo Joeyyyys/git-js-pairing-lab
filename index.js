0b5e0a1ef6ca7b43ec54306cb97de619a9c94663
EC2 Default User <ec2-user@ip-172-31-10-101.us-east-2.compute.internal>
2023-03-29 18:27:33 +0000

    made multply of Three into a variable, num is the parameter,made the sum to equal to 100 basically after the 100 the forlook will stop and set i to equal 1 and made sure that i should be less than and equal to the num which will be 100 in the forloop and if it excutes all this it will continue to the next number  and return the sum and at he end i console the multiply of Three to 100 because i want it to stop at 100

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ function fiveToHundred(){
  }
  console.log(fiveToHundred())
  
- 
+
+
+//2 Write a function `multiplesOfThree` that console logs all positive numbers to 100 that is a multiple of 3.
+ function multiplyOfThree (num){
+let sum = 100
+for(Let i = 1; i > num; i++){
+if(i % 3 === 0){
+    sum += i 
+}
+ return sum   
+}
+console.log multiplyOfThree((100))
